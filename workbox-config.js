9c343ce3174f2a2d5bf494a773310d91a9f18ec0
Joseph Goh <josephgoh7@gmail.com>
2023-11-16 09:34:31 +0800

    pwa test 9

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: 'build/',
+	globPatterns: [
+		'**/*.{html,js,css,json}'
+	],
+	swDest: 'build/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
